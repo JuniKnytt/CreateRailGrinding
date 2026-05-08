d2c49ce54ddfa2076ac430a19c929c460d5250cc
Juni Knytt <42306249+JuniKnytt@users.noreply.github.com>
2026-05-08 12:22:23 -0700

    added todo

--- src/main/java/net/juniknytt/createrailgrinding/Config.java
+++ src/main/java/net/juniknytt/createrailgrinding/Config.java
@@ -3,6 +3,9 @@ package net.juniknytt.createrailgrinding;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public final class Config {
+
+    //TO DO SORT THESE OUT, THEY ARE OKAY AS THEY ARE
+
     private static final ModConfigSpec.Builder CLIENT_BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.BooleanValue DEBUG_MODE = CLIENT_BUILDER
